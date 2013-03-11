f8373a16a2d426acc6cdba6fbf7fddba0840e962
lldyllml <lldyllml@gmail.com>
2013-03-11 10:16:44 +0800

    优化MyEditText，并增加常用颜色工具类Colors.java

--- src/me/xiaopan/androidlibrary/widget/MyEditText.java
+++ src/me/xiaopan/androidlibrary/widget/MyEditText.java
@@ -1,9 +1,8 @@
-package test.widget;
+package me.xiaopan.androidlibrary.widget;
 
-import me.xiaopan.androidlibrary.R;
+import me.xiaopan.androidlibrary.util.Colors;
 import me.xiaopan.androidlibrary.util.TextUtils;
 import android.content.Context;
-import android.content.res.TypedArray;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.text.Editable;
@@ -15,10 +14,8 @@ import android.widget.EditText;
 
 public class MyEditText extends EditText {
 	private boolean fromMe;
-	private String editName;//编辑器名称
 	private Drawable clearDrawable;//清除图标
 	private MyTextWatcher myTextWatcher;
-	private MyOnTouchListener myOnTouchListener;
 	private OnTouchListener onTouchListener;
 	
 	public MyEditText(Context context, AttributeSet attrs, int defStyle) {
@@ -41,34 +38,24 @@ public class MyEditText extends EditText {
 	 * @param attrs
 	 */
 	private void init(AttributeSet attrs){
-		//获取编辑器名称
-		if(attrs != null){
-			TypedArray typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.MyEditText);
-			editName = typedArray.getString(R.styleable.MyEditText_editName);
-			typedArray.recycle();
-		}
-		//尝试初始化编辑器名称
-		tryInitEditName();
-		
-		//初始化文本改变监听器和触摸监听器
-		myTextWatcher = new MyTextWatcher();
-		myOnTouchListener = new MyOnTouchListener();
+		//初始化触摸监听器
 		fromMe = true;
-		setOnTouchListener(myOnTouchListener);
+		setOnTouchListener(new MyOnTouchListener());
+
+		//尝试初始化编辑器名称
+		tryInitEditName((String) getContentDescription());
 		
-		//获取清除图标
-		clearDrawable = getCompoundDrawables()[2];
 		//尝试初始化清除图标
-		tryInitClearDrawable();	
+		tryInitClearDrawable(getCompoundDrawables()[2]);	
 	}
 	
 	/**
 	 * 尝试初始化编辑器名称
 	 */
-	private void tryInitEditName(){
+	private void tryInitEditName(String editName){
 		//名字不为null也不为空就执行初始化
 		if(editName != null && !"".equals(editName)){
-			Drawable nameDrawable = new BitmapDrawable(getContext().getResources(), TextUtils.getTextBitmap(getContext(), editName, getContext().getResources().getColor(R.color.base_gray_dark), getTextSize()));
+			Drawable nameDrawable = new BitmapDrawable(getContext().getResources(), TextUtils.getTextBitmap(getContext(), editName, Colors.GRAY_DARK, getTextSize()));
 			Drawable[] drawables = getCompoundDrawables();
 			setCompoundDrawablesWithIntrinsicBounds(nameDrawable, drawables[1], drawables[2], drawables[3]);
 		}
@@ -77,9 +64,13 @@ public class MyEditText extends EditText {
 	/**
 	 * 尝试初始化清除图标
 	 */
-	private void tryInitClearDrawable(){
-		if(clearDrawable != null){
+	private void tryInitClearDrawable(Drawable clearDrawable){
+		this.clearDrawable = clearDrawable;
+		if(this.clearDrawable != null){
 			//添加文本改变监听器
+			if(myTextWatcher == null){
+				myTextWatcher = new MyTextWatcher();
+			}
 			addTextChangedListener(myTextWatcher);
 			//刺激一下刚刚添加的内容改变监听器，这样做的效果是当在布局中设置的有默认值时，会立即显示出清除图标
 			setText(getText().toString());
@@ -120,21 +111,21 @@ public class MyEditText extends EditText {
 		@Override
 		public boolean onTouch(View v, MotionEvent event) {
 			boolean result = false;
-			if(clearDrawable != null){
+			if(clearDrawable != null && getCompoundDrawables()[2] != null){
 				switch (event.getAction()) {
-				case MotionEvent.ACTION_UP:
-					int curX = (int) event.getX();//转换触摸位置的X坐标
-					int curY = (int) event.getY();//转换触摸位置的Y坐标
-					int rightDrawableTop = (getHeight() - clearDrawable.getIntrinsicHeight()) / 2;//计算清除图标的Top值
-					//如果点击范围正好在清除图标上并且文本编辑器的内容不为空
-					if(curX >= getWidth() - (clearDrawable.getIntrinsicWidth() + getPaddingRight()) && 
+					case MotionEvent.ACTION_UP:
+						int curX = (int) event.getX();//转换触摸位置的X坐标
+						int curY = (int) event.getY();//转换触摸位置的Y坐标
+						int rightDrawableTop = (getHeight() - clearDrawable.getIntrinsicHeight()) / 2;//计算清除图标的Top值
+						//如果点击范围正好在清除图标上并且文本编辑器的内容不为空
+						if(curX >= getWidth() - (clearDrawable.getIntrinsicWidth() + getPaddingRight()) && 
 							curX <= getWidth() - getPaddingRight() && 
 							curY >= rightDrawableTop && 
 							curY <= getHeight() - rightDrawableTop && 
 							getText().toString().length() > 0) {
-						setText("");
-						result = true;
-					}	
+							setText("");
+							result = true;
+						}	
 					break;
 				}
 			}
@@ -156,13 +147,11 @@ public class MyEditText extends EditText {
 	}
 
 	public String getEditName() {
-		return editName;
+		return (String) getContentDescription();
 	}
 
 	public void setEditName(String editName) {
-		this.editName = editName;
-		tryInitEditName();
-		invalidate();
+		tryInitEditName(editName);
 	}
 
 	public Drawable getClearDrawable() {
@@ -170,8 +159,6 @@ public class MyEditText extends EditText {
 	}
 
 	public void setClearDrawable(Drawable clearDrawable) {
-		this.clearDrawable = clearDrawable;
-		tryInitClearDrawable();
-		invalidate();
+		tryInitClearDrawable(clearDrawable);
 	}
 }
