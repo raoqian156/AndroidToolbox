8bce985a406bad8136713578be63a2e20d18b63f
xiaopan <xiaopan@xiaopan-pc>
2013-03-31 18:54:23 +0800

    更换天气测试接口

--- src/test/net/request/WeatherRequest.java
+++ src/test/net/request/WeatherRequest.java
@@ -1,7 +1,9 @@
 package test.net.request;
 
+import me.xiaopan.androidlibrary.net.HostAddress;
 import me.xiaopan.androidlibrary.net.Path;
 import me.xiaopan.androidlibrary.net.Request;
 
+@HostAddress("http://www.weather.com.cn/data/cityinfo")
 @Path("101010100.html")
 public class WeatherRequest extends Request {}
