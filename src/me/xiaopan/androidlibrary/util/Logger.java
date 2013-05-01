bea9a8252bb88e98fa519be9a0ee16b56a3db9d8
XIAOPAN <XIAOPAN@XIAOPAN-PC>
2013-05-01 17:13:41 +0800

    Logger工具类增加输出到本地文件的方法

--- src/me/xiaopan/androidlibrary/util/Logger.java
+++ src/me/xiaopan/androidlibrary/util/Logger.java
@@ -1,69 +1,81 @@
 package me.xiaopan.androidlibrary.util;
 
+import java.io.File;
+import java.io.IOException;
+
+import me.xiaopan.javalibrary.util.FileUtils;
+
 import android.util.Log;
 
 public class Logger {
 	private static final String LOG_TAG = "LOG";
 	public static final boolean ENABLE = true;
+	public static File outFile;
 	
-	public static final void v(String tag, String msg){
+	public static final void v(String tag, String logContent){
 		if(ENABLE){
-			Log.v(tag, msg);
+			Log.v(tag, logContent);
+			outToFile(tag+" "+logContent);
 		}
 	}
 	
-	public static final void v(String msg){
-		v(LOG_TAG, msg);
+	public static final void v(String logContent){
+		v(LOG_TAG, logContent);
 	}
 	
-	public static final void d(String tag, String msg){
+	public static final void d(String tag, String logContent){
 		if(ENABLE){
-			Log.d(tag, msg);
+			Log.d(tag, logContent);
+			outToFile(tag+" "+logContent);
 		}
 	}
 	
-	public static final void d(String msg){
-		d(LOG_TAG, msg);
+	public static final void d(String logContent){
+		d(LOG_TAG, logContent);
 	}
 	
-	public static final void i(String tag, String msg){
+	public static final void i(String tag, String logContent){
 		if(ENABLE){
-			Log.i(tag, msg);
+			Log.i(tag, logContent);
+			outToFile(tag+" "+logContent);
 		}
 	}
 	
-	public static final void i(String msg){
-		i(LOG_TAG, msg);
+	public static final void i(String logContent){
+		i(LOG_TAG, logContent);
 	}
 	
-	public static final void w(String tag, String msg){
+	public static final void w(String tag, String logContent){
 		if(ENABLE){
-			Log.w(tag, msg);
+			Log.w(tag, logContent);
+			outToFile(tag+" "+logContent);
 		}
 	}
 	
-	public static final void w(String msg){
-		w(LOG_TAG, msg);
+	public static final void w(String logContent){
+		w(LOG_TAG, logContent);
 	}
 	
-	public static final void e(String tag, String msg){
+	public static final void e(String tag, String logContent){
 		if(ENABLE){
-			Log.e(tag, msg);
+			Log.e(tag, logContent);
+			outToFile(tag+" "+logContent);
 		}
 	}
 	
-	public static final void e(String msg){
-		e(LOG_TAG, msg);
+	public static final void e(String logContent){
+		e(LOG_TAG, logContent);
 	}
 	
-	public static final void wtf(String tag, String msg){
+	public static final void wtf(String tag, String logContent){
 		if(ENABLE){
-			Log.wtf(tag, msg);
+			Log.wtf(tag, logContent);
+			outToFile(tag+" "+logContent);
 		}
 	}
 	
-	public static final void wtf(String msg){
-		wtf(LOG_TAG, msg);
+	public static final void wtf(String logContent){
+		wtf(LOG_TAG, logContent);
 	}
 	
 	public static final void wtf(String tag, Throwable th){
@@ -76,9 +88,43 @@ public class Logger {
 		wtf(LOG_TAG, th);
 	}
 	
-	public static final void wtf(String tag, String msg, Throwable th){
+	public static final void wtf(String tag, String logContent, Throwable th){
 		if(ENABLE){
-			Log.wtf(tag, msg, th);
+			Log.wtf(tag, logContent, th);
+			outToFile(tag+" "+logContent);
+		}
+	}
+	
+	public static final boolean outToFile(String logContent){
+		if(ENABLE && outFile != null){
+			if(outFile.exists()){
+				try {
+					FileUtils.writeStringByLine(outFile, logContent, true);
+					return true;
+				} catch (IOException e) {
+					e.printStackTrace();
+					return false;
+				}
+			}else{
+				File parentFile = outFile.getParentFile();
+				//如果父目录不存在就创建，如果创建失败了就直接结束
+				if(!parentFile.exists() && !parentFile.mkdirs()){
+					return false;
+				}
+				try {
+					if(outFile.createNewFile()){
+						FileUtils.writeStringByLine(outFile, logContent, true);
+						return true;
+					}else{
+						return false;
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+					return false;
+				}
+			}
+		}else{
+			return false;
 		}
 	}
 }
