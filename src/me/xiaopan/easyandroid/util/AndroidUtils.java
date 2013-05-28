977b5c82cf875c07cfbab6721017ff801adca87e
XIAOPAN <XIAOPAN@XIAOPAN-PC>
2013-05-28 11:29:07 +0800

    在AndroidUtils中增加dp和px相互转换的工具方法

--- src/me/xiaopan/easyandroid/util/AndroidUtils.java
+++ src/me/xiaopan/easyandroid/util/AndroidUtils.java
@@ -568,4 +568,24 @@ public class AndroidUtils {
 	public static String getDeviceId(Context context){
 		return ((TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE)).getDeviceId();
 	}
+	
+	/**
+	 * dp单位转换为px
+	 * @param context 上下文，需要通过上下文获取到当前屏幕的像素密度
+	 * @param dpValue dp值
+	 * @return
+	 */
+	public static int dp2px(Context context, float dpValue){
+		return (int)(dpValue * (context.getResources().getDisplayMetrics().density) + 0.5f);
+	}
+	
+	/**
+	 * px单位转换为dp
+	 * @param context 上下文，需要通过上下文获取到当前屏幕的像素密度
+	 * @param pxValue px值
+	 * @return
+	 */
+	public static int px2dp(Context context, float pxValue){
+		return (int)(pxValue / (context.getResources().getDisplayMetrics().density) + 0.5f);
+	}
 }
