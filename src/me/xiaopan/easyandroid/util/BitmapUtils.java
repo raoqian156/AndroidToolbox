0ccda1a9500558a49c763329ea4cb7d609c4411f
ixiaopan <i@xiaopan.me>
2013-08-19 17:15:31 +0800

    增加最佳缩放比例计算方法

--- src/me/xiaopan/easyandroid/util/BitmapUtils.java
+++ src/me/xiaopan/easyandroid/util/BitmapUtils.java
@@ -1059,4 +1059,44 @@ public class BitmapUtils {
 			return null;
 		}
 	}
+	
+	/**
+	 * 计算BitmapFactory.Options合适的缩放比例
+	 * @param options
+	 * @param minSideLength 用于指定最小宽度或最小高度
+	 * @param maxNumOfPixels 最大尺寸，由最大宽高相乘得出
+	 * @return
+	 */
+	public static int computeSampleSize(BitmapFactory.Options options, int minSideLength, int maxNumOfPixels) {
+	    int initialSize = computeInitialSampleSize(options, minSideLength, maxNumOfPixels);
+	    int roundedSize;
+	    if (initialSize <= 8) {
+	        roundedSize = 1;
+	        while (roundedSize < initialSize) {
+	            roundedSize <<= 1;
+	        }
+	    } else {
+	        roundedSize = (initialSize + 7) / 8 * 8;
+	    }
+	    return roundedSize;
+	}
+	
+	private static int computeInitialSampleSize(BitmapFactory.Options options, int minSideLength, int maxNumOfPixels) {
+	    double w = options.outWidth;
+	    double h = options.outHeight;
+	    int lowerBound = (maxNumOfPixels == -1) ? 1 : (int) Math.ceil(Math.sqrt(w * h / maxNumOfPixels));
+	    int upperBound = (minSideLength == -1) ? 128 : (int) Math.min(Math.floor(w / minSideLength), Math.floor(h / minSideLength));
+	    if (upperBound < lowerBound) {
+	        return lowerBound;
+	    }
+
+	    if ((maxNumOfPixels == -1) &&
+	            (minSideLength == -1)) {
+	        return 1;
+	    } else if (minSideLength == -1) {
+	        return lowerBound;
+	    } else {
+	        return upperBound;
+	    }
+	}
 }
