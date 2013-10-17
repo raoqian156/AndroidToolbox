f373b84802936595e5657c44236e716296278be9
ixiaopan <i@xiaopan.me>
2013-10-17 16:24:03 +0800

    增加从Assest中获取位图的功能

--- src/me/xiaopan/easy/android/util/AssetsUtils.java
+++ src/me/xiaopan/easy/android/util/AssetsUtils.java
@@ -6,6 +6,9 @@ import java.nio.charset.Charset;
 
 import me.xiaopan.easy.java.util.IOUtils;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Rect;
 
 /**
  * Assets文件操作工具箱
@@ -19,7 +22,7 @@ public class AssetsUtils {
 	 * @param charset 转换编码
 	 * @return
 	 */
-	public static final String readString(Context context, String fileName, Charset charset){
+	public static final String getString(Context context, String fileName, Charset charset){
 		InputStream inputStream = null;
 		try {
 			inputStream = context.getAssets().open(fileName);
@@ -45,7 +48,59 @@ public class AssetsUtils {
 	 * @param fileName 文件名
 	 * @return
 	 */
-	public static final String readString(Context context, String fileName){
-		return readString(context, fileName, Charset.defaultCharset());
+	public static final String getString(Context context, String fileName){
+		return getString(context, fileName, Charset.defaultCharset());
+	}
+	
+	/**
+	 * 获取位图
+	 * @param context 上下文
+	 * @param fileName 文件名称
+	 * @param outPadding 输出位图的内边距
+	 * @param options 加载选项
+	 * @return
+	 */
+	public static final Bitmap getBitmap(Context context, String fileName, Rect outPadding, BitmapFactory.Options options){
+		try {
+			InputStream inputStream = context.getAssets().open(fileName);
+			Bitmap bitmap = BitmapFactory.decodeStream(inputStream, outPadding, options);
+			inputStream.close();
+			return bitmap;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	/**
+	 * 获取位图
+	 * @param context 上下文
+	 * @param fileName 文件名称
+	 * @param outPadding 输出位图的内边距
+	 * @return
+	 */
+	public static final Bitmap getBitmap(Context context, String fileName, Rect outPadding){
+		return getBitmap(context, fileName, outPadding, null);
+	}
+	
+	/**
+	 * 获取位图
+	 * @param context 上下文
+	 * @param fileName 文件名称
+	 * @param options 加载选项
+	 * @return
+	 */
+	public static final Bitmap getBitmap(Context context, String fileName, BitmapFactory.Options options){
+		return getBitmap(context, fileName, null, options);
+	}
+	
+	/**
+	 * 获取位图
+	 * @param context 上下文
+	 * @param fileName 文件名称
+	 * @return
+	 */
+	public static final Bitmap getBitmap(Context context, String fileName){
+		return getBitmap(context, fileName, null, null);
 	}
 }
