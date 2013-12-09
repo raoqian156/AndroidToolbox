c1d893972311b86c2b3c69059edced9819c9d5ab
ixiaopan <i@xiaopan.me>
2013-12-09 14:16:02 +0800

    增加视图刷新处理器ViewRefreshHandler.java

---
+++ src/me/xiaopan/easy/android/util/ViewRefreshHandler.java
@@ -0,0 +1,47 @@
+package me.xiaopan.easy.android.util;
+
+import android.os.Handler;
+import android.view.View;
+
+/**
+ * 视图刷新处理器
+ */
+public class ViewRefreshHandler implements Runnable{
+	private View view;
+	private Handler handler;
+	private int intervalMillis;
+	
+	public ViewRefreshHandler(View view, Handler handler, int intervalMillis) {
+		this.view = view;
+		this.handler = handler;
+		this.intervalMillis = intervalMillis;
+	}
+	
+	public ViewRefreshHandler(View view, Handler handler) {
+		this(view, handler, 30);
+	}
+	
+	public ViewRefreshHandler(View view, int intervalMillis) {
+		this(view, new Handler(), intervalMillis);
+	}
+	
+	public ViewRefreshHandler(View view) {
+		this(view, new Handler(), 30);
+	}
+	
+	@Override
+	public void run(){
+		view.invalidate();
+		handler.postDelayed(this, intervalMillis);
+	}
+	
+	public void start(){
+		handler.removeCallbacks(this);
+		handler.post(this);
+	}
+	
+	public void stop(){
+		handler.removeCallbacks(this);
+		view.invalidate();
+	}
+}
